4d98698e73ce6a1cf4a8a35ad077015ffdf6d59c
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2024-10-19 07:49:23 +0530

    update  admin Auth  controller in verifyEmail method

--- src/controller/adminAuth.controller.js
+++ src/controller/adminAuth.controller.js
@@ -4,6 +4,7 @@ import bcrypt from "bcrypt";
 import UserAdmin from "../models/Useradmin.model.js";
 import generateVerificationToken from "../utils/generateVerificationToken.js";
 import { sendVerificationMail } from "../MailServices/Email.services.js";
+import EnvConfig from "../config/Config.js";
 
 
 
@@ -69,48 +70,52 @@ res.status(201).json({
 }
 
 
-const VerifyAuthentication= async(req,res,next)=>{
-  
-  const {code}=req.body;
-  // const { email } = req.params;
-
+const VerifyAuthentication = async (req, res, next) => {
+  const { code } = req.body; // Get the code from request body
 
   try {
-        const  UserFind= await UserAdmin.findOne({
-        //  email,
-          verificationToken:code,
-          verificationTokenExpiresAt: { $gt: Date.now() }  // $gt operator checks if the verificationTokenExpiresAt (expiration date) is greater than the current time (Date.now()). If it is, the token has not expired.
-        });
+   
+    const UserFind = await UserAdmin.findOne({
+      verificationToken: code,
+      verificationTokenExpiresAt: { $gt: Date.now() }, // Token expiration validation
+    });
 
-        if(!UserFind){
-          const error =createHttpError(401,"Invalid Verification Code !")
-          return next(error);
-        }
     
-        UserFind.isVerified=true;
-        UserFind.verificationToken=undefined;
-        UserFind.verificationTokenExpiresAt=undefined;
-     
-      await UserFind.save();
+    if (!UserFind) {
+      const error = createHttpError(401, "Invalid or Expired Verification Code!");
+      return next(error);
+    }
 
-      res.status(203).json({
-        message:"user successfuly Verified his account !",
+    
+    UserFind.isVerified = true;
+    UserFind.verificationToken = undefined; // Clear verification token
+    UserFind.verificationTokenExpiresAt = undefined; // Clear expiration time
+
+    
+    await UserFind.save();
+
+    
+    res.status(200).json({
+      message: "User successfully verified their account!",
+      user: {
         ...UserFind._doc,
-        password:undefined,
-
-     })
-    
+        password: undefined, 
+      },
+    });
   } catch (error) {
-    console.log("Error Occures Verification Email  code.",error);
+    console.log("Error occurred during email verification:", error);
     return next(error);
-
   }
-}
-
+};
 
 const LoggedIn=async(req,res,next)=>{
    
   const {email,mobileNumber,password}=req.body;
+
+  console.log("Email :",email);
+  console.log("mobileNumber :",mobileNumber);
+  console.log("password :",password);
+  
 
   try {
 
@@ -129,24 +134,34 @@ const LoggedIn=async(req,res,next)=>{
       return next(error);
     }
 
-    const isMatched=bcrypt.compare(password,LoggedUser.password);
-
+    const isMatched= await bcrypt.compare(password,LoggedUser.password); //always used await in bcrypt 
+ 
+  //  console.log("password matched :", isMatched);
+  
     if(!isMatched){
       const error =createHttpError(401,"Invalid creadntial , password not matched !");
       return next(error);
     }
-     
-
+    const verificationToken= Math.floor(100000 + Math.random() * 900000).toString();
+    
+    LoggedUser.verificationToken=verificationToken;
+    LoggedUser.verificationTokenExpiresAt= Date.now() + 24 * 60 * 60 * 1000, //24 hours
+    // LoggedUser.isVerified=false;
     LoggedUser.lastLogin=Date.now();
-
-    generateVerificationToken(res,LoggedUser._id);
+    
+    // generateVerificationToken(res,LoggedUser._id);
+     if(isMatched){
+      await generateVerificationToken(res,LoggedUser._id);
+     }
 
     await LoggedUser.save();
 
+    const mailRes=await sendVerificationMail(LoggedUser.email,verificationToken,next); //sending verifaction mail
     res.status(203).json({
       success:true,
       ...LoggedUser._doc,
       password:undefined,
+      mailresponse: mailRes.success?mailRes:{success:false},
     })
 
     
@@ -161,7 +176,13 @@ const LoggedIn=async(req,res,next)=>{
 
 const LoggedOut= async(req,res,next)=>{
  
- res.clearCookie("AuthToken");
+  const isProduction=EnvConfig.ENV_APP_MOOD==="production";
+
+  res.clearCookie('AuthToken', {
+    httpOnly: true,
+    secure: isProduction, // secure flag for production
+    sameSite: 'strict',
+  });
  res.status(201).json({
   success:true,
   message:"user successfuly Logged out !",
@@ -180,7 +201,7 @@ const checkAuth=async(req,res,next)=>{
 
   const { userID } = req.user; 
   
-//  console.log(req);
+ console.log("User id  in CheckAuth : ", userID);
  
   if(!userID){
     const error=createHttpError(404,"User id Not found");
